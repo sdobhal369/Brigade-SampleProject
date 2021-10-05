eeb6e8f94c66374514f2574fbcbf48173f9dd895
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2021-10-05 16:29:50 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express()
-
-app.get('/', (req, res) => res.send('Congratulation! Web Application is Working...'));
-
-app.listen(3000, () => {
-    console.log('My REST API running on port 3000!');
-})
