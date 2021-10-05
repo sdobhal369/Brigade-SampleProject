89489d602992b410162a07ffb0cd9b889033e5d1
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2021-10-05 17:12:00 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -2,9 +2,9 @@ const { events, Job, Groups } = require("brigadier");
 events.on("exec", () => {
   var build = new Job("build-job");
   build.image = "alpine:3.4";
-  build.tasks = ["RUN apt-get update", 
-                 "RUN apt-get install -y python", 
-                 "ADD hello.py /home/hello.py",  
+  build.tasks = ["apt-get update", 
+                 "apt-get install -y python", 
+                 "hello.py /home/hello.py",  
                  "CMD [/home/hello.py]",
                  "docker build -t myimage:10  ."];
   build.run();
