3434378c2edc25b088211b7d1acfbbb40221920d
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2021-10-05 17:15:00 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -2,11 +2,7 @@ const { events, Job, Groups } = require("brigadier");
 events.on("exec", () => {
   var build = new Job("build-job");
   build.image = "alpine:3.4";
-  build.tasks = ["apt-get update", 
-                 "apt-get install -y python", 
-                 "hello.py /home/hello.py",  
-                 "CMD [/home/hello.py]",
-                 "docker build -t myimage:10  ."];
+  build.tasks = ["docker build -t myimage:10 python-deploy ."];
   build.run();
 });
 
