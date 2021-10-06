3ee6d8a309e106e78a0b930797f565abb1e101fa
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2021-10-06 15:49:11 +0530

    Delete brigade.js

--- brigade.js
+++
@@ -1,7 +0,0 @@
-const { events, Job, Groups } = require("brigadier");
-events.on("exec", () => {
-  var build = new Job("build-job");
-  build.tasks = ["docker build -t myimage:10 python-deploy ."];
-  build.run();
-});
-
