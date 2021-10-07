194eaecabaf696b45a39cd47b2dc874c826bcd9f
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2021-10-07 12:44:14 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -2,7 +2,6 @@ const { events, Job } = require("brigadier");
 events.on("exec", (e,project) => {
   var dockerBuild = new Job("docker-packaging");
   dockerBuild.image = "docker:dind";
-  dockerBuild.docker.enabled = false;
   dockerBuild.privileged = true;
   dockerBuild.env = {
     DOCKER_DRIVER: "overlay",
@@ -14,9 +13,9 @@ events.on("exec", (e,project) => {
     "dockerd-entrypoint.sh &",
     "sleep 30",
     "cd /src",
-    "docker build -t sdobhal369/brigade-test:40 .",
+    "docker build -t sdobhal369/brigade-test:50 .",
     "docker login -u $DOCKER_USER -p $DOCKER_PASS",
-    "docker push sdobhal369/brigade-test:40"
+    "docker push sdobhal369/brigade-test:50"
   ];
   
   dockerBuild.run();
