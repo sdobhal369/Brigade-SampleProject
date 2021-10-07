efa9dda639fc7b96c52fca85092bea161659c7d7
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2021-10-07 10:48:13 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,7 +1,7 @@
 const { events, Job } = require("brigadier");
 events.on("exec", () => {
   var dockerBuild = new Job("docker-build");
-  dockerBuild.image = "docker:dind"
+  dockerBuild.image = "docker:dind";
   dockerBuild.privileged = true;
   
   dockerBuild.env = {
