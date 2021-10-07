bb1eef20a0f2ccc6935aa6ae142f7ce6368210a4
Shubham Dobhal <89004127+sdobhal369@users.noreply.github.com>
2021-10-07 12:06:02 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -4,8 +4,8 @@ events.on("exec", () => {
   dockerBuild.image = "docker:dind";
   dockerBuild.docker.enabled = true;
   
-  dockerBuild.env.DOCKER_USER = project.secrets.dockerLogin;
-  dockerBuild.env.DOCKER_PASS = project.secrets.dockerPass;
+  dockerBuild.env.DOCKER_USER = project.secrets.dockerLogin
+  dockerBuild.env.DOCKER_PASS = project.secrets.dockerPass
   
   dockerBuild.tasks = [
     "dockerd-entrypoint.sh &",
